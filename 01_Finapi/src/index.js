de1376a610e718b77ad028da427a86e2198f72be
Rafael Pablo Massocato <rafaelmassocato@gmail.com>
2022-09-01 18:41:59 -0300

    middleware one

--- 01_Finapi/src/index.js
+++ 01_Finapi/src/index.js
@@ -1,11 +1,52 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.json());
 
-return response.send('Teste get');
+const customers = [];
 
+function verifyIfExistsAccountCPF( request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find( (customer) => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({ error: "Customer not found"});
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
+app.post("/account", ( request, response ) => {
+    const { cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({error: "Customer already exists!"});
+    };
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: [],
+    });
+
+return response.status(201).send();
+
+});
+
+app.get("/statement", verifyIfExistsAccountCPF, ( request, response ) => {
+    const { customer } = request;
+
+    return response.json(customer.statement);
 });
 
 app.listen(3333);
