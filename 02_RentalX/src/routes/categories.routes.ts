b5585d3f1cfeaea5c76d37a210b5aad7221e4f8d
Rafael Pablo Massocato <rafaelmassocato@gmail.com>
2022-09-06 16:25:36 -0300

    model to category and category route adjusts

---
+++ 02_RentalX/src/routes/categories.routes.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prettier/prettier */
+import { Router } from "express";
+import { v4 as uuidV4 } from "uuid";
+
+import { Category } from "../model/Category";
+
+const categoriesRoutes = Router();
+
+const categories: Category[] = [];
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const category = new Category();
+  
+    Object.assign(category, {
+        name,
+        description,
+        created_at: new Date(),
+    })
+
+  categories.push(category);
+
+  return response.status(201).json( category );
+});
+
+export { categoriesRoutes };
